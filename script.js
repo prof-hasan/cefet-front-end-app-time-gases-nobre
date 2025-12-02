dec7fdf170be4fb3c0eda4a08a7117e65aaa644d
gabrielaalvesdsantos <santosgabriela3390@gmail.com>
2025-12-02 15:10:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const boneco = document.querySelector('.boneco');
+const tubo = document.querySelector('.tubo');
 
 const jump = () => {
     boneco.classList.add('jump');
 
-    setTime
+    setTimeout(() => {
+      boneco.classList.remove('jump');
+    }, 500);
 
 }
+
+const loop = setInterval(() => {
+    const tuboPosition = tubo.offsetLeft;
+    const bonecoPosition = boneco.offsetBottom;
+
+    console.log(bonecoPosition)
+   
+    if (tuboPosition <= 120) {
+        tubo.style.animation = 'none';
+        tubo.style.left = `${tuboPosition}px`;
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
