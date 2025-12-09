4989ca44aaee77e8f98b259074296588d2d0d4d0
gabrielaalvesdsantos <santosgabriela3390@gmail.com>
2025-12-09 14:32:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const boneco = document.querySelector('.boneco');
 const tubo = document.querySelector('.tubo');
 
+// Função de pulo
 const jump = () => {
     boneco.classList.add('jump');
 
     setTimeout(() => {
-      boneco.classList.remove('jump');
+        boneco.classList.remove('jump');
     }, 500);
+};
 
-}
-//minuto 23:10
+// Loop do jogo (detecção de colisão)
 const loop = setInterval(() => {
     const tuboPosition = tubo.offsetLeft;
-    const bonecoPosition = +window.getComputedStyle(boneco).bottom.replace('')
+    const bonecoPosition = parseFloat(
+        window.getComputedStyle(boneco).bottom
+    );
 
-    console.log(bonecoPosition);
-   
-    if (tuboPosition <= 120) {
+    const bateu = tuboPosition < 120 && tuboPosition > 0 && bonecoPosition < 80;
+
+    if (bateu) {
         tubo.style.animation = 'none';
         tubo.style.left = `${tuboPosition}px`;
+
+        boneco.src = 'batendo.png';
+        boneco.style.width = '150px';
+        boneco.style.marginLeft = '20px';
+        boneco.style.bottom = '0';
+
+        document.getElementById('game-over').style.display = 'block';
+
+        clearInterval(loop);
     }
 }, 10);
 
+// Evento para pular
 document.addEventListener('keydown', jump);
