a831d9ebac4070146003b9740a61a5a825ceda91
FelipeCrispim14 <felipesantosgomes66@gmail.com>
2025-12-12 16:26:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 const boneco = document.querySelector('.boneco');
 const tubo = document.querySelector('.tubo');
 
-// Função de pulo
 const jump = () => {
     boneco.classList.add('jump');
 
     setTimeout(() => {
-        boneco.classList.remove('jump');
+      boneco.classList.remove('jump');
     }, 500);
-};
 
-// Loop do jogo (detecção de colisão)
+}
+//minuto 23:10
 const loop = setInterval(() => {
     const tuboPosition = tubo.offsetLeft;
-    const bonecoPosition = parseFloat(
-        window.getComputedStyle(boneco).bottom
-    );
+    const bonecoPosition = +window.getComputedStyle(boneco).bottom.replace('')
 
-    const bateu = tuboPosition < 120 && tuboPosition > 0 && bonecoPosition < 80;
-
-    if (bateu) {
+    console.log(bonecoPosition);
+   
+    if (tuboPosition <= 120) {
         tubo.style.animation = 'none';
         tubo.style.left = `${tuboPosition}px`;
-
-        boneco.src = 'batendo.png';
-        boneco.style.width = '150px';
-        boneco.style.marginLeft = '20px';
-        boneco.style.bottom = '0';
-
-        document.getElementById('game-over').style.display = 'block';
-
-        clearInterval(loop);
     }
 }, 10);
 
-// Evento para pular
 document.addEventListener('keydown', jump);
